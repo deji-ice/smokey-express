638bd6efe0ad40b3be171b8760b987fd2f49be2a
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-05-15 12:33:34 +0100

    Revert "users route"
    This reverts commit dc08d3741a1fba95c796e65a5367426dfdf9a52c.

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,29 +2,11 @@ import express from "express"
 import db from "../db/conn.js"
 
 const userRoutes = express.Router()
- 
+
 userRoutes.get("/users", async (req, res)=>{
     let collection = await db.collection("smokey")
     let results = await collection.find({}).limit(20).toArray()
     res.send(results)
 })
-
-userRoutes.post('/users', async (req, res)=>{
-    let data = req.body
-    if(!data) res.send('please send something').status(400)
-    data.date = new Date()
-    let collection = await db.collection("smokey")
-    let results = await collection.insertOne(data) 
-
-    if(results){
-        res.send('user saved successfully').status(201)
-    } else if(!results){
-        res.send('server error').status(500)  
-    } 
-
-})
-
-
-
 
 export default userRoutes
